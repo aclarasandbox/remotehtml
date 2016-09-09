60ddb8c9a2473216b8f29d8a4c733eb78ed90b77
aclarasandbox <pgarge@aclara.com>
2016-09-09 11:49:14 -0400

    Add files via upload

--- acl-remote-header.js
+++ acl-remote-header.js
@@ -1,3 +1,3 @@
-﻿aclaraJQuery.get("https://cdn.rawgit.com/aclarasandbox/remotehtml/master/bootstrap-header-menu.htm", function (header) {
+﻿$.get("https://cdn.rawgit.com/aclarasandbox/remotehtml/master/bootstrap-header-menu.htm", function (header) {
     aclaraJQuery("#acl-remote-header").replace(header);
 }, 'html');  
